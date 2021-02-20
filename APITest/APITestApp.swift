9020ec77b861298fefd38cb8f24a2493b48ff2f9
Abijah Kajabika <34766858+AbijahKaj@users.noreply.github.com>
2021-02-20 15:21:27 +0300

    Initial Commit

---
+++ APITest/APITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  APITestApp.swift
+//  APITest
+//
+//  Created by admin on 20.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct APITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
